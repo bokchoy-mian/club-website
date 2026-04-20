03afa905ff718cfa296964e6e6ae5848d4ac6d46
xjl <1335172962@qq.com>
2026-04-20 22:15:33 +0800

    初始化项目：校园社团官网

---
+++ main.js
@@ -0,0 +1,11 @@
+// 引入 Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// 引入 Bootstrap Icons CSS
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+// 引入 Bootstrap JS
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+// 你的其他代码可以写在这里
+console.log('项目初始化完成');
