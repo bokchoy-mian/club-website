3d6e2495bb6af8128b40cdec65aa5090c3345d08
bochoy <1219043831@njupt.edu.cn>
2026-04-25 22:35:12 +0800

    github CI

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
 // vite.config.js
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import htmlInclude from 'vite-plugin-html-include';
 
 export default defineConfig({
-  base: '/club-website/',
+  plugins: [
+    htmlInclude() //配置插件
+  ],
+  base: '/club-website/', //配置基础路径
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',  //vite编译时入口文件
+        news: 'news.html' //vite编译时入口文件
+      }
+    }
+  }
 })
